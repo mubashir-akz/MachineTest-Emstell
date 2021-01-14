dba8005e29873720a8acf9e7767184530ca5d618
Mubashir-akz <mubashir.jr520@gmail.com>
2021-01-14 23:18:49 +0530

    adding routes

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next)=> {
   res.render('login', { title: 'Express' });
 });
+
+// Post :-- LoginValidation
+router.post('/login',()=>{
+
+})
 
 router.get('/register',(req,res)=>{
   res.render('register')
